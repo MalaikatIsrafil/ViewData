489e0f6517d93fb20933d409db1fc352fb5fe0c8
Riski Afendi <riskistar2703@gmail.com>
2024-11-09 16:46:06 +0700

    Last Commit

---
+++ app/src/main/java/id/ac/polbeng/riskiafendi/viewdata/MainActivity.kt
@@ -0,0 +1,27 @@
+package id.ac.polbeng.riskiafendi.viewdata
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.ac.polbeng.riskiafendi.viewdata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnViewList.setOnClickListener{
+            startActivity(Intent(applicationContext, ListviewActivity::class.java))
+        }
+
+        binding.btnRecylerView.setOnClickListener{
+            startActivity(Intent(applicationContext, RecylerActivity::class.java))
+        }
+
+
+    }
+}
